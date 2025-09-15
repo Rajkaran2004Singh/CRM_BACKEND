42f546c19f3dc5c4fb66e1b3bf70e6fbcd586300
Rajkaran2004Singh <126346368+Rajkaran2004Singh@users.noreply.github.com>
2025-09-15 13:10:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.set("trust proxy", 1);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
